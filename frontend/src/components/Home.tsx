ba2da2e2c272922528a9bfa82dfdfb13a33dedb8
Scott <Scottyjmt13@gmail.com>
2025-03-05 18:20:16 -0700

    set up react router. render dummy data to home. basic style

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div id="home-block">
+      <h1>Astray Among the Stars</h1>
+      <h2>About</h2>
+      <h2>Release Notes</h2>
+      <h2>Register</h2>
+      <h2>Login</h2>
+      <h2>PLAY</h2>
+      <Link></Link>
+    </div>
+  )
+}
+
+export default Home;
