7e6bf83a2aa8262a733241a7597ada9a67d7dac7
Scott <Scottyjmt13@gmail.com>
2025-03-07 13:56:23 -0700

    add token state. pass props to register.tsx and home.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./components/Home.tsx"
 import About from "./components/About.tsx";
 import ReleaseNotes from "./components/ReleaseNotes.tsx";
 import Register from "./components/Register.tsx";
 import Login from "./components/Login.tsx";
 
-const App = () => {
+const App: React.FC = () => {
+  const [token, setToken] = useState<string>("")
 
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Home token={token}/>}/>
       <Route path="about" element={<About />}/>
       <Route path="releasenotes" element={<ReleaseNotes />}/>
-      <Route path="register" element={<Register />}/>
+      <Route path="register" element={<Register token={token} setToken={setToken}/>}/>
       <Route path="login" element={<Login />}/>
     </Routes>
   )
